8c69d7b2de88e83d57f548b7e49d2a4c03ac73be
Evelina Poniatajeva <71027917+Evelina2641@users.noreply.github.com>
2021-02-24 20:08:12 +0200

    Registration front js

---
+++ blog_front/scripts/registration.js
@@ -0,0 +1,36 @@
+// Global Variables
+
+let form = document.querySelector('form')
+
+// Functions
+let registration = async (e) => {
+    e.preventDefault();
+    // Variables
+    let username = document.getElementById('username').value
+    let name = document.getElementById('name').value
+    let surname = document.getElementById('surname').value
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    
+    let data = {
+        username,
+        name,
+        surname,
+        password,
+        email
+    }
+    try{
+        const response = await fetch('http://localhost:3000/blog/author/signup', {
+            method: 'POST',
+            headers: {"Content-Type": 'application/json'},
+            body: JSON.stringify(data)
+        })
+        if(response.status != 200) throw await response.json()
+        // window.location.href = './login.html' CIA VELIAU KAI LOGIN ATSIRAS
+        window.location.href = '../pages/main.html'
+    }catch(e) {
+       console.log(e);
+    }
+}
+// events
+form.addEventListener('submit', registration)
