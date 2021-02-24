11db7b5ba42d1a9e20e964a6dcd7b10e745fe0a7
Evelina Poniatajeva <71027917+Evelina2641@users.noreply.github.com>
2021-02-24 22:20:42 +0200

    Pataisyta Logout funkcija

--- blog_front/scripts/index.js
+++ blog_front/scripts/index.js
@@ -2,26 +2,28 @@
 let token;
 let logoutButton = document.querySelector('.logout')
 
+window.addEventListener('DOMContentLoaded', () => {
+    token = localStorage.getItem('author-auth')
+    if(!token) window.location.href = '../pages/main.html'
+})
 // Functions
 let logout = async () => {
     localStorage.removeItem('author-auth')
     try {
-        let response = await fetch('http://localhost:3000/blog/author/logout', {
+        const response = await fetch('http://localhost:3000/blog/author/logout', {
             method: 'GET',
             headers: {"Content-Type": 'application/json',
-            "auth-token": token}
+            "author-auth": token}
         })
         if(response.status != 200) throw await response.json()
+        
         window.location.href = '../pages/main.html'
+        
     }catch(e) {
-        alert(e)
+        alert(e);
     }
 }
 
 // Events 
-window.addEventListener('DOMContentLoaded', () => {
-    let token = localStorage.getItem('author-auth')
-    if(!token) window.location.href = '../pages/login.html'
-})
 
 logoutButton.addEventListener('click', logout)
